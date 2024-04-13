e08d764441925c3e4666c65344b67dcec4475a83
nikkero <131234301+nikkero@users.noreply.github.com>
2024-04-13 20:54:04 +0600

    app

---
+++ app.js
@@ -0,0 +1,23 @@
+// ...импорты и настройки
+const {mainRouteController, defaultRouteController, gameRouteController, voteRouteController} = require("./controllers")
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    switch (url) {
+        case "/":
+            mainRouteController(res, "/index.html", ".html");
+        break;
+        case "/game":
+            gameRouteController(res);
+        break;
+        case "/vote":
+            voteRouteController(req, res);
+        break;
+        default:
+            defaultRouteController(res, url)
+        break;
+    }
+  });
+
+server.listen("3005")
---
+++ appModules/http-utils/index.js
@@ -0,0 +1,9 @@
+const staticFile = require("./static-file.js");
+const mimeTypes = require("./mime-types.js");
+const parseBody = require("./parse-body.js");
+
+module.exports = {
+    staticFile,
+    mimeTypes,
+    parseBody 
+}
